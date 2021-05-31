4fb83c9cba1617ef92517ad4efeb6f7b507d17c3
doanja <doanja@plu.edu>
2021-05-31 14:01:07 -0700

    initial commit with nextjs and tailwind installed

---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+import 'tailwindcss/tailwind.css';
+export default function Home() {
+  return (
+    <div>
+      <h1 className='text-lg text-blue-500'>this is the home page</h1>
+    </div>
+  );
+}
