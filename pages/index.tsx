e54c8942aa3ea16c20fe44d74c21b30bc72c89cd
doanja <doanja@plu.edu>
2021-06-01 14:47:12 -0700

    dockerized application with docker-compose

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import 'tailwindcss/tailwind.css';
 export default function Home() {
   return (
     <div>
-      <h1 className='text-lg text-blue-500'>this is the home page</h1>
+      <h1 className='text-lg text-blue-500'>Home</h1>
     </div>
   );
 }
