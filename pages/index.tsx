3e4d796642940ccefe0e0a796c56ee578182ec0a
doanja <doanja@plu.edu>
2021-06-01 14:54:58 -0700

    added next.config.js to allow hot reloading

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import 'tailwindcss/tailwind.css';
 export default function Home() {
   return (
     <div>
-      <h1 className='text-lg text-blue-500'>Home</h1>
+      <h1 className='text-lg text-blue-500'>Hot Reload is Working</h1>
     </div>
   );
 }
