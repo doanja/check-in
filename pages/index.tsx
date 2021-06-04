eaaf5875130606151a2875086674e976922de339
doanja <doanja@plu.edu>
2021-06-04 15:26:02 -0700

    fixed issue with tailwindcss

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,7 @@
-// import 'tailwindcss/tailwind.css';
-
 export default function Home() {
   return (
     <div>
-      <h1 className='text-lg text-blue-500'>Home</h1>
+      <h1 className='text-lg text-red-500'>Home</h1>
     </div>
   );
 }
