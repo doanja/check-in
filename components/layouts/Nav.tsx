44938307025d2e9480da2cddf3976d05cd7f44b8
doanja <doanja@plu.edu>
2021-07-09 16:48:15 -0700

    updated navabar

--- components/layouts/Nav.tsx
+++ components/layouts/Nav.tsx
@@ -1,18 +1,21 @@
 import { useState } from 'react';
-import NavButton from './NavButton';
+
 import NavLink from './NavLink';
 
 const Navbar = () => {
   const [showMenu, setShowMenu] = useState(true);
+  const closeMenu = () => {
+    setShowMenu(!showMenu);
+  };
 
   return (
-    <nav className='flex flex-wrap items-center justify-between p-5 bg-blue-200'>
+    <nav>
       {/* logo */}
-      <img src='https://seeklogo.com/images/B/bulbasaur-logo-8A27924C02-seeklogo.com.png' alt='ACME' width='120' />
+      <img src='https://www.wilsonfamilychiropracticcenter.net/wp-content/uploads/2018/12/placeholder-logo-2.png' alt='ACME' width='120' />
 
       {/* hamburger */}
       <div className='flex md:hidden'>
-        <button onClick={() => setShowMenu(!showMenu)}>
+        <button onClick={closeMenu}>
           <img
             className={`${showMenu ? 'block' : 'hidden'}`}
             src='https://img.icons8.com/fluent-systems-regular/2x/menu-squared-2.png'
@@ -23,18 +26,15 @@ const Navbar = () => {
         </button>
       </div>
 
-      {/* TODO: clicking links or button toggles menu off */}
-
       {/* nav links */}
       <div
-        className={` ${showMenu && 'hidden'} md:flex w-full md:w-auto text-right text-bold mt-5 md:mt-0 border-t-2 border-blue-900 md:border-none`}>
+        className={` ${showMenu && 'hidden'} md:flex w-full md:w-auto text-right text-bold mt-5 md:mt-0 border-t-2 border-blue-900 md:border-none`}
+        onClick={closeMenu}>
         <NavLink linkTo='' linkText='home' />
         <NavLink linkTo='signup' linkText='sign-up' />
         <NavLink linkTo='signin' linkText='sign-in' />
+        <NavLink linkTo='waitlist' linkText='waitlist' />
       </div>
-
-      {/* create acc button */}
-      <NavButton showMenu={showMenu} linkTo='waitlist' linkText='waitlist' />
     </nav>
   );
 };
