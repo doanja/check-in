5c0bd4707d283f98cb01e32c0234a6fd66fc14cd
doanja <doanja@plu.edu>
2021-09-03 12:41:30 -0700

    added cursor-pointer to table rows

--- components/WaitlistTableRow.tsx
+++ components/WaitlistTableRow.tsx
@@ -7,10 +7,10 @@ interface WaitlistTableRowProps {
 const WaitlistTableRow = ({ user, allowEdits, toggleIsCheckedIn }: WaitlistTableRowProps) => {
   return (
     <tr className='bg-gray-800' onClick={() => allowEdits && toggleIsCheckedIn(user.id)}>
-      <td className={`${user.isCheckedIn && 'bg-gray-600'}`}>
+      <td className={`${user.isCheckedIn && 'bg-gray-600'} cursor-pointer`}>
         <p className={`${user.isCheckedIn && 'line-through'}`}>{user.name}</p>
       </td>
-      <td className={`${user.isCheckedIn && 'bg-gray-600'}`}>
+      <td className={`${user.isCheckedIn && 'bg-gray-600'} cursor-pointer`}>
         <p className={`${user.isCheckedIn && 'line-through'}`}> {user.checkInTime}</p>
       </td>
     </tr>
